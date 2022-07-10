815e0e853f8b691e77c2ab657424d7c111fec786
eunveloper <hye_7991@naver.com>
2022-07-11 03:00:28 +0900

    _20220711 clean-code remark

---
+++ src/com/study/books/cleancode/remark/obj/Module.java
@@ -0,0 +1,10 @@
+package com.study.books.cleancode.remark.obj;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class Module {
+    public ArrayList getDependSubsystems() {
+        return null;
+    }
+}
---
+++ src/com/study/books/cleancode/remark/obj/SubSysMod.java
@@ -0,0 +1,7 @@
+package com.study.books.cleancode.remark.obj;
+
+public class SubSysMod {
+    public String getSubSystem() {
+        return null;
+    }
+}
