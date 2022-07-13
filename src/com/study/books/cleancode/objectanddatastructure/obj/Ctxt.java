e7a0b5bfe90b2abeb3f5ad8d7e7f2399e89dbc20
eunveloper <hye_7991@naver.com>
2022-07-14 01:30:22 +0900

    _20220714 clean-code object and data structure

---
+++ src/com/study/books/cleancode/objectanddatastructure/obj/Ctxt.java
@@ -0,0 +1,17 @@
+package com.study.books.cleancode.objectanddatastructure.obj;
+
+import java.io.File;
+
+public class Ctxt {
+    public Ctxt getOptions() {
+        return this;
+    }
+
+    public Ctxt getScratchDir() {
+        return this;
+    }
+
+    public String getAbsolutePath() {
+        return null;
+    }
+}
